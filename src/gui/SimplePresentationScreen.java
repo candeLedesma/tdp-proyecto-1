6df3645b3e48fe3addbe23ecf418b67e2ffd567a
candeLedesma <candelaledesmapm@gmail.com>
2022-08-27 18:20:21 -0300

    Implementacion completada

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -9,6 +9,13 @@ import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,8 +24,17 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JLabel lblLU;
+	private JTextField textFieldLU;
+	private JTextField textFieldApellido;
+	private JTextField textFieldNombre;
+	private JTextField textFieldEmail;
+	private JTextField textFieldGithub;
+	private JLabel lblfecha;
 
 	public SimplePresentationScreen(Student studentData) {
+		setBackground(Color.WHITE);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
@@ -36,10 +52,75 @@ public class SimplePresentationScreen extends JFrame {
 	
 	private void init() {
 		// Tabbed Pane to student personal data
+		
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
 		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		tabInformation.setLayout(null);
+		
+		lblLU = new JLabel("LU");
+		lblLU.setBounds(12, 12, 60, 15);
+		tabInformation.add(lblLU);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(12, 39, 60, 15);
+		tabInformation.add(lblApellido);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(12, 66, 60, 15);
+		tabInformation.add(lblNombre);
+		
+		JLabel lblEmail = new JLabel("E-mail");
+		lblEmail.setBounds(12, 93, 60, 15);
+		tabInformation.add(lblEmail);
+		
+		JLabel lblGithub = new JLabel("Github URL");
+		lblGithub.setBounds(12, 120, 78, 15);
+		tabInformation.add(lblGithub);
+		
+		textFieldLU = new JTextField();
+		textFieldLU.setBounds(109, 10, 264, 19);
+		tabInformation.add(textFieldLU);
+		textFieldLU.setColumns(10);
+		String lu= Integer.toString(studentData.getId());
+		textFieldLU.setText(lu);
+		
+		textFieldApellido = new JTextField();
+		textFieldApellido.setBounds(109, 37, 264, 19);
+		tabInformation.add(textFieldApellido);
+		textFieldApellido.setColumns(10);
+		textFieldApellido.setText(studentData.getLastName());
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setBounds(109, 64, 264, 19);
+		tabInformation.add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+		textFieldNombre.setText(studentData.getFirstName());
+
+		
+		textFieldEmail = new JTextField();
+		textFieldEmail.setBounds(109, 91, 264, 19);
+		tabInformation.add(textFieldEmail);
+		textFieldEmail.setColumns(10);
+		textFieldEmail.setText(studentData.getMail());
+		
+		textFieldGithub = new JTextField();
+		textFieldGithub.setBounds(108, 118, 265, 19);
+		tabInformation.add(textFieldGithub);
+		textFieldGithub.setColumns(10);
 		contentPane.add(tabbedPane, BorderLayout.WEST);
+		textFieldGithub.setText(studentData.getGithubURL());
+		
+		
+		JLabel lblImagen = new JLabel("");
+		lblImagen.setIcon(new ImageIcon(SimplePresentationScreen.class.getResource("/images/chickenIcon.png")));
+		contentPane.add(lblImagen, BorderLayout.EAST);
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		lblfecha = new JLabel("Esta ventana fue generada el "+ dtf.format(now));
+		contentPane.add(lblfecha, BorderLayout.SOUTH);
+		setVisible(true);
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,20 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
+		Student estudiante = new Student(135330,"Ledesma","Candela","candelaledesmapm@gmail.com", "https://github.com/candeLedesma", "pepe");
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	try {
+					SimplePresentationScreen window = new SimplePresentationScreen(estudiante);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
             }
         });
     }
